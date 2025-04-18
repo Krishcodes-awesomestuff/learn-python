879f9f28ea5941d5f035071ac7eb32e3b171f1ee
Krishnaprasath Venkadesan <230801093@rajalakshmi.edu.in>
2025-04-18 16:34:06 +0530

    Added Episode 2 - Variables and f-string formatting

---
+++ Episode_2.py
@@ -0,0 +1,15 @@
+#variable = A container for a value (e.g. String, Integer, Float, List, Dictionary)
+
+#These are string variables
+first_name = "Krishna"
+food = "Dosa"
+print(f"Hello, {first_name}")
+print(f"you like {food}")
+
+#integer
+age = 25
+quantity = 3
+num_of_students = 30
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"your class has {num_of_students} students")
